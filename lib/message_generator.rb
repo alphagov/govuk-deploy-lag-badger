7c94d30ed250271b2d1395c87778a7866c76d8b6
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2018-11-30 16:32:50 +0000

    Correct typo in variable name

--- lib/message_generator.rb
+++ lib/message_generator.rb
@@ -29,23 +29,23 @@ class MessageGenerator
       commit.to_h.dig(:commit, :author, :name)
     }.uniq.to_sentence
 
-    unpdeployed_pull_requests = compare[:commits].select { |commit|
+    undeployed_pull_requests = compare[:commits].select { |commit|
       commit[:commit][:message].start_with?("Merge pull request")
     }.compact
 
-    return unless unpdeployed_pull_requests.first
+    return unless undeployed_pull_requests.first
 
-    merge_date_of_oldest_pull_request = unpdeployed_pull_requests.first[:commit][:committer][:date]
+    merge_date_of_oldest_pull_request = undeployed_pull_requests.first[:commit][:committer][:date]
 
     return unless merge_date_of_oldest_pull_request < (Time.now - 7.days)
 
     seconds_ago = ((Time.now - merge_date_of_oldest_pull_request).abs).round
     days_ago = seconds_ago / 1.day
 
-    if unpdeployed_pull_requests.size == 1
+    if undeployed_pull_requests.size == 1
       "- <#{RELEASE_APP_PATH}#{app_slug}|#{app_name}> has <#{compare[:html_url]}|1 undeployed pull request> which was merged #{days_ago} days ago. It includes commits by #{committer_names}."
     else
-      "- <#{RELEASE_APP_PATH}#{app_slug}|#{app_name}> has <#{compare[:html_url]}|#{unpdeployed_pull_requests.size} undeployed pull requests>, the oldest of which was merged #{days_ago} days ago. It includes commits by #{committer_names}."
+      "- <#{RELEASE_APP_PATH}#{app_slug}|#{app_name}> has <#{compare[:html_url]}|#{undeployed_pull_requests.size} undeployed pull requests>, the oldest of which was merged #{days_ago} days ago. It includes commits by #{committer_names}."
     end
   end
 
